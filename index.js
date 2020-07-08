2222d69910e79dea2fd9a6eba57cae7837ac4318
Tereshka <tereshka07@gmail.com>
2020-07-08 14:58:31 +0900

    some changes

---
+++ index.js
@@ -0,0 +1,29 @@
+const micro = require('micro');
+
+const server = micro(async (req, res) => {
+  if (req.method !== 'POST') {
+    return 'Server is running';
+  }
+
+  const { request, session } = await micro.json(req);
+  return {
+    response: {
+      text: session.new
+        ? 'Привет'
+        : `Вы сказали: ${request.command}`,
+      tts: session.new
+        ? '<speaker audio="alice-music-harp-1.opus">Привет, я ваш голосовой помощник'
+        : `Вы сказали: ${request.command}`,
+      tts: '<speaker audio="alice-music-harp-1.opus">Привет, я ваш голосовой помощник',
+      buttons: [
+        { title: 'Здравствуй', hide: true },
+        { title: 'Как дела?', hide: true },
+      ],
+      end_session: false
+    },
+    version: '1.0'
+  };
+});
+
+const PORT = 3030;
+server.listen(PORT, () => console.log(`Server started on http://localhost:${PORT}, tunnel: http://localhost:4040`));
