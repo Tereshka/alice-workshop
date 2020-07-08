2746ba969ba5fddf97537c3b1b79c238823291f6
Tereshka <tereshka07@gmail.com>
2020-07-08 15:16:57 +0900

    add some math

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const micro = require('micro');
 const replies = require('./src/replies');
 
+/**
+ * @type {{number1: number, number2: number}}
+ */
+let question;
+
 const server = micro(async (req, res) => {
   if (req.method !== 'POST') {
     return 'Server is running';
@@ -9,13 +14,40 @@ const server = micro(async (req, res) => {
   const { request, session } = await micro.json(req);
   const response = session.new
     ? replies.welcome()
-    : replies.repeatUserCommand(request.command);
+    : checkAnswer(request.command);;
 
   return {
     response,
     version: '1.0'
   };
 });
+
+function checkAnswer(command) {
+  if (!question) {
+    question = generateQuestion();
+    return replies.firstQuestion(question);
+  }
+
+  if (isCorrectAnswer(command)) {
+    question = generateQuestion();
+    return replies.correctAnswer(question);
+  }
+
+  return replies.incorrectAnswer(question);
+}
+
+function isCorrectAnswer(command) {
+  const matches = command.match(/[0-9]+/);
+  const correctAnswer = question.number1 + question.number2;
+  return matches && Number(matches[0]) === correctAnswer;
+}
+
+function generateQuestion() {
+  return {
+    number1: Math.ceil(Math.random() * 20),
+    number2: Math.ceil(Math.random() * 20),
+  };
+}
 
 const PORT = 3030;
 server.listen(PORT, () => console.log(`Server started on http://localhost:${PORT}, tunnel: http://localhost:4040`));
--- src/replies.js
+++ src/replies.js
@@ -3,11 +3,10 @@
  */
 exports.welcome = () => {
   return {
-    text: 'Привет',
-    tts: '<speaker audio="alice-music-harp-1.opus">Привет, я ваш голосовой помощник',
+    text: 'Привет. Я ваш новый учитель математики. Начинаем урок?',
+    tts: '<speaker audio="alice-music-harp-1.opus">Привет. Я ваш новый учитель математики. Начинаем урок?',
     buttons: [
-      { title: 'Здравствуй', hide: true },
-      { title: 'Как дела?', hide: true },
+      { title: 'Начинаем', hide: true },
     ],
     end_session: false
   };
@@ -21,6 +20,47 @@ exports.welcome = () => {
 exports.repeatUserCommand = command => {
   return {
     text: `Вы сказали: ${command}`,
+    end_session: false
+  };
+};
+
+/**
+ * Первый вопрос пользователю.
+ *
+ * @param {Number} number1
+ * @param {Number} number2
+ */
+exports.firstQuestion = ({ number1, number2 }) => {
+  return {
+    text: `Сколько будет ${number1} + ${number2} = ?`,
+    tts: `Сколько будет ${number1} + ${number2}`,
+    end_session: false
+  };
+};
+/**
+ * Реакция на неправильный ответ.
+ *
+ * @param {Number} number1
+ * @param {Number} number2
+ */
+exports.incorrectAnswer = ({ number1, number2 }) => {
+  return {
+    text: `Неверно. Попробуй еще раз: ${number1} + ${number2} = ?`,
+    tts: `Неверно. Попробуй еще раз: ${number1} + ${number2}`,
+    end_session: false
+  };
+};
+
+/**
+ * Реакция на правильный ответ.
+ *
+ * @param {Number} number1
+ * @param {Number} number2
+ */
+exports.correctAnswer = ({ number1, number2 }) => {
+  return {
+    text: `Правильно! Следующий вопрос: ${number1} + ${number2} = ?`,
+    tts: `<speaker audio="alice-sounds-human-crowd-6.opus">Правильно! Следующий вопрос: ${number1} + ${number2}`,
     end_session: false
   };
 };
