ee509dc958839befc523ef5b11d432b065908cce
Tereshka <tereshka07@gmail.com>
2020-07-08 12:12:15 +0900

    add files

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const micro = require('micro');
+
+const server = micro(async (req, res) => {
+  return {
+    response: {
+      text: 'Привет',
+      end_session: false
+    },
+    version: '1.0'
+  };
+});
+
+const PORT = 3030;
+server.listen(PORT, () => console.log(`Server started on http://localhost:${PORT}, tunnel: http://localhost:4040`));
